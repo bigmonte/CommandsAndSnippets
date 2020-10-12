74b7d8ba6e2f60d826bccc9a6cdea1e59e6d343a
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-12 09:18:26 +0100

    Simple Console APP to access Database using EF to easily make tests

--- tools/CommandsAndSnippetsTools/Program.cs
+++ tools/CommandsAndSnippetsTools/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using CommandsAndSnippetsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace CommandsAndSnippetsTools
 {
@@ -6,7 +10,20 @@ namespace CommandsAndSnippetsTools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TODO use secrets
+            
+            string conString = "Server=localhost,1433\\Catalog=sql1;Database=sql1;User=SA;Password=<YourStrong@Passw0rd>;";
+
+            var optionsBuilder = new DbContextOptionsBuilder<DBContext>();
+            
+            optionsBuilder.UseSqlServer(conString);
+
+            using (var context = new DBContext(optionsBuilder.Options))
+            {
+                Console.WriteLine($"{context.CommandItems.First()?.Platform}");
+            }
         }
+
+
     }
 }
