835c8c3286f06b1d7def976764e0e15bb58a1734
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-12 08:19:24 +0100

    Added tools and new projects for LINQ tests with XML and so on

---
+++ tools/CommandsAndSnippetsTools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandsAndSnippetsTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
