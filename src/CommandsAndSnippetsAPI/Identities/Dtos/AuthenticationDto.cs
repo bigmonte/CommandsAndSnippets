2ec07e03cc4f5bbf2466dcfa96908ebf6015ae65
bigmonte <34067397+bigmonte@users.noreply.github.com>
2020-10-18 09:08:16 +0100

    Removed unnecessary file

--- src/CommandsAndSnippetsAPI/Identities/Dtos/AuthenticationDto.cs
+++ src/CommandsAndSnippetsAPI/Identities/Dtos/AuthenticationDto.cs
@@ -1,11 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace CommandsAndSnippetsAPI.Dtos.User
 {
     public sealed class AuthenticationDto
     {
         public string Token { get; init; }
         
-        public string Success { get; init; }
-        
-        public string ErrorMessage { get; init; }
+        public IdentityResult IdentityResult { get; init; }
     }
 }
